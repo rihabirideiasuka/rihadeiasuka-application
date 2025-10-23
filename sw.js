65417b1d92dd7a64a23d67ecbb28b70429209a5d
rihabirideiasuka <rihabirideiasuka2@gmail.com>
2025-10-24 08:24:22 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'buy-sale-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './install.html',
+  './manifest.webmanifest',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const { request } = e;
+  e.respondWith(
+    caches.match(request).then(res => res || fetch(request))
+  );
+});
